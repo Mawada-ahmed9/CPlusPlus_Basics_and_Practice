c996d17ab7f6d144d8821322b50114303cd3fe25
Mawada-ahmed9 <da9630794@gmail.com>
2026-09-05 22:57:29 +0300

    Add program to fill and print array with random numbers

---
+++ ArrayWithRandomNumbers.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+int ReadPositiveNumber(string massage){
+    int number;
+    do
+    {
+        cout << massage;
+        cin >> number;
+    } while (number <= 0);
+    return number;   
+}
+int FillArrayWithRandomNumbers(int from, int to){
+    return rand()% (to - from +1) + from;
+}
+void ReadArray(int array[100],int number){
+    
+    for (int i = 0; i < number; i++){
+        array[i] = FillArrayWithRandomNumbers(1, 100);   
+    }
+}
+void Printarrays(int array[100],int number){
+    for (int i = 0; i < number; i++){
+     cout << array[i] << " ";        
+    }
+}
+int main (){
+    srand((unsigned)time(NULL));
+    int array[100] , number;
+    number= ReadPositiveNumber("enter your number: ");
+    ReadArray(array, number);
+    Printarrays(array, number);
+       
+    return 0;
+    
+} 
