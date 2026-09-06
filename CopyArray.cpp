cf9a22ccb8d321fd9dbcc087c4b82ad930bdf6f3
Mawada-ahmed9 <da9630794@gmail.com>
2026-09-06 22:29:39 +0300

    Refactor array handling functions in CopyArray.cpp

--- CopyArray.cpp
+++ CopyArray.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <cmath>
 using namespace std;
 
 int ReadPositiveNumber(string massage){
@@ -9,37 +10,45 @@ int ReadPositiveNumber(string massage){
     {
         cout << massage;
         cin >> number;
-    } while (number <= 0);
+    } while (number < 0);
     return number;   
 }
 int RandomNumber(int from, int to){
-    return rand()% (to - from +1) + from;
+    return rand()%(to - from +1) +from;
 }
-void FillArrayWithRandomNumbers(int array[100],int number){
-    for (int i = 0; i < number; i++){
-        array[i] =RandomNumber(1, 100);   
+void FillArray(int array[100], int number){
+    for(int i = 0; i < number; i++){
+        array[i] = RandomNumber(1, 100);
     }
 }
-void PrintArray(int array[100],int number){ 
-    for (int i = 0; i < number; i++){
-        cout << array[i]<< " " ;   
+void AddArrayElement(int number, int array2[100], int &length){
+    array2[length] = number;
+}
+void CopyArray(int array[100], int array2[100], int number){
+    for(int i =0; i < number; i++){
+        AddArrayElement(array[i],array2,i);
+    }
+}
+void PrintArray(int array[100], int length){
+    for(int i = 0; i < length; i++){
+        cout << array[i]<< " ";
+        
     }cout << endl;
 }
-void CopyArray(int array[100], int arr2[100], int number){
-    for (int i = 0; i < number; i++){
-        arr2[i] = array[i];    
-    }cout << endl;
-}
+
+
 int main (){
     srand((unsigned)time(NULL));
-    int array[100] , number, arr2[100];
-    number= ReadPositiveNumber("enter your number: ");
-    FillArrayWithRandomNumbers(array, number);
+    int array[100], number = ReadPositiveNumber("enter your massge "), array2[100];
+
+    FillArray(array, number);
+    cout << "Array 1 elements: " endl<<;
     PrintArray(array, number);
-    CopyArray(array, arr2, number);
-    PrintArray(arr2, number);
-    
-    
+
+    CopyArray(array, array2, number);
+cout << "Array 2 elements: " endl<<;
+    PrintArray(array2, number);
+
     return 0;
     
 } 
