40ef78684be929bb96ef3facc31b73d208eabbf9
Mawada-ahmed9 <da9630794@gmail.com>
2026-09-19 17:57:10 +0300

    Implement functions to read and print a 2D array

---
+++ 2DArray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+void ReadArray(int x[10][10])
+{
+    
+    for(int i =1; i <= 10; i++)
+    {
+        for(int j = 1; j <= 10; j++)
+        {
+            x[i-1][j-1] = i * j;
+        }
+    }
+}
+void PrintArray(int x[10][10])
+{
+    
+    for(int i =0; i < 10; i ++)
+    {
+        for(int j =0; j < 10; j++)
+        {
+            printf("%0*d ", 2, x[i][j]);
+        }cout << endl;
+    }
+}
+int main(){
+    int x[10][10];
+    ReadArray(x);
+    PrintArray(x);
+return 0;
+}
